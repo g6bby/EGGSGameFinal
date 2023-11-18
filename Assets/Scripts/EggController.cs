7bd2560d8f817661a247fd3254c84509e9e1483e
g6bby <nabag123@newschool.edu>
2023-11-17 23:55:48 -0500

    fixed player look movement

--- Assets/Scripts/EggController.cs
+++ Assets/Scripts/EggController.cs
@@ -6,6 +6,12 @@ public class EggController : MonoBehaviour
 {
     public float rollSpeed = 5f;
 
+    void Start()
+    {
+        Cursor.visible = false; 
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
     void Update()
     {
         HandleInput();
@@ -13,11 +19,26 @@ public class EggController : MonoBehaviour
 
     void HandleInput()
     {
+        // Get the input for movement
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
+        // Get the camera's forward and right vectors
+        Vector3 cameraForward = Camera.main.transform.forward;
+        Vector3 cameraRight = Camera.main.transform.right;
 
+        // Project the vectors onto the horizontal plane (Y = 0)
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        // Normalize the vectors to ensure consistent speed in all directions
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        // Calculate the movement direction based on input and camera orientation
+        Vector3 movement = (cameraForward * verticalInput + cameraRight * horizontalInput).normalized;
+
+        // Apply force to make the player move
         GetComponent<Rigidbody>().AddForce(movement * rollSpeed);
     }
 }
