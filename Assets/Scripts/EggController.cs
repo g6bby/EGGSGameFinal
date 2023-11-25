35b501d8b4b346fe1dd57064fbf820e2d39d5aaf
g6bby <nabag123@newschool.edu>
2023-11-25 02:19:35 -0500

    added hills

--- Assets/Scripts/EggController.cs
+++ Assets/Scripts/EggController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class EggController : MonoBehaviour
 {
-    public float rollSpeed = 5f;
+    [SerializeField]
+    private float rollSpeed = 8f;
 
     void Start()
     {
